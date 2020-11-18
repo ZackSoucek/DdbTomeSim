38f99dabd267a715756ead529913d25e55457639
BuildTools <zacksoucek@gmail.com>
2020-11-17 21:10:57 -0800

    creator with testing pickling

--- Creator.py
+++ Creator.py
@@ -1,8 +1,13 @@
 from Challenge import *
 import pickle
 
-
+level1 = open("Data\Baseline\Tome1\level1", 'wb')
 chal1 = Challenge("Test", tome = 1, level = 1, side = Side.KILLER, value = 2500, state = State.INCOMPLETE,
-          position = (200, 100), description = "kill 5 survivors 11 times")
+                  position = (200, 100), description = "kill 5 survivors 11 times")
 print(chal1)
-
+pickle.dump(chal1, level1)
+level1.close()
+level1 = open("Data\Baseline\Tome1\level1", 'rb')
+unpickled = pickle.load(level1)
+level1.close()
+print(unpickled)
